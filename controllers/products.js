f1d92d4b23406ab89f7488f5b3c9b31d218fccd1
chryzcode <alabaolanrewaju13@gmail.com>
2023-12-16 23:02:02 +0100

    push

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,13 @@
-const Product = require('../models/products')
-
-const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products testing route" });
-};
+const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find();
+  const products = await Product.find({ name: "vase table" });
   res.status(200).json({ products, nbHits: products.length });
-   
+};
+
+const getAllProducts = async (req, res) => {
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
