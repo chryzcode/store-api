919a637a28a302dc4bc4bbbc8099fd951da4021b
chryzcode <alabaolanrewaju13@gmail.com>
2023-12-17 08:17:20 +0100

    push

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,28 @@
+const products = require("../models/products");
 const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ name: "vase table" });
+  const products = await Product.find({}).sort('-name price')
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company, name, sort } = req.query
+  const queryObject ={}
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  } 
+  let result =  Product.find(queryObject);
+  if (sort) {
+    products = products.sort
+  }
+  const products = await result
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -14,3 +30,4 @@ module.exports = {
   getAllProducts,
   getAllProductsStatic,
 };
+ 
